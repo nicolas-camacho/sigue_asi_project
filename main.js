2d17c60363ee656e200e3fc0a6dd2927954f8ecd
nicolas-camacho <ncamacho1@cuc.edu.co>
2019-11-24 17:00:35 -0500

    :sparkles: Funcionalidad lista

---
+++ main.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer')
+
+let bigArray = []
+
+let posQuestion = [{
+  type: 'input',
+  name: 'sPos',
+  message: "Insert next positions",
+}]
+
+let fQuestion = [{
+  type: 'list',
+  choices: ['Show big Array', 'Insert next'],
+  message: "Select an option",
+  name: 'option'
+}]
+
+
+const init = () => {
+  inquirer.prompt(fQuestion).then(answers => {
+    if(answers['option'] === 'Show big Array') {
+      console.log(bigArray);
+      init()
+    }else{
+      insertSpos()
+    }
+  })
+}
+
+
+const insertSpos = () => {
+  let sPos = []
+  inquirer.prompt(posQuestion).then(answers => {
+    sPos = answers['sPos']
+    console.log(sPos);
+    if(bigArray.includes(sPos)){
+      console.log('Ya existe <3!!');
+      console.log(`Posicicion ${bigArray.indexOf(sPos) + 1}`);
+      init()
+    }else{
+      bigArray.push(sPos)
+      init()
+    }
+  })
+}
+
+init()
+
+
